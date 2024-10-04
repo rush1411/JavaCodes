70883ea65667867281fe0b680c769e28780ea1fb
rushikesh gaikwad <116288483+rush1411@users.noreply.github.com>
2024-10-04 15:54:25 +0530

    Update second.java

--- second.java
+++ second.java
@@ -3,5 +3,10 @@ public class code
 	public static void main(String []args)
 	{
 		System.out.println("Hello Everyone.....");
+		System.out.println("Hello students...:);
+		int a=10;
+		int b=20;
+		int c;
+		c=a+b;
 	}
 }
