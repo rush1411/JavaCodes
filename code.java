89b3bf187810c894cd3e81ba3b6d62ef0f4913ec
Rush <rushig173@gmail.com>
2024-10-04 16:25:11 +0530

    commit for update

--- code.java
+++ code.java
@@ -3,10 +3,12 @@ public class code
 	public static void main(String []args)
 	{
 		System.out.println("Hello Everyone.....");
-		System.out.println("Hello students...:);
+		System.out.println("Hello students...");
 		int a=10;
 		int b=20;
 		int c;
+		
+		System.out.println("hello");
 		c=a+b;
 		System.out.println(c);
 	}
